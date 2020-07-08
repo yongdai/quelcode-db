82876d6f5853dc03c8094fed7a3c5ceee213153d
YONGDAI HAN <YONG@YONGDAIs-Macbook-Pro.local>
2020-07-08 14:16:41 +0900

    理論値を一致に変更

--- db/db-challenge3/db-009/db-009.sql
+++ db/db-challenge3/db-009/db-009.sql
@@ -3,6 +3,6 @@ INNER JOIN chatposts
 ON chatrooms.chat_room_id = chatposts.chat_room_id
 INNER JOIN users
 ON chatposts.owner_user_id = users.user_id
-WHERE chatposts.is_deleted != 1 
-AND   users.is_deleted != 1
+WHERE chatposts.is_deleted = 0 
+AND   users.is_deleted = 0
 GROUP BY chat_name;
