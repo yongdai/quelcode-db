2a9492f46b065442dda31b4eaf9342763fced60a
YONGDAI HAN <YONG@YONGDAIs-Macbook-Pro.local>
2020-07-08 14:19:38 +0900

    投稿数の降順での要件を追加

--- db/db-challenge3/db-009/db-009.sql
+++ db/db-challenge3/db-009/db-009.sql
@@ -1,8 +1,9 @@
-SELECT chat_name, COUNT(chat_post_id) AS '投稿数' FROM chatrooms
+SELECT COUNT(chat_post_id) AS '投稿数', chat_name FROM chatrooms
 INNER JOIN chatposts
 ON chatrooms.chat_room_id = chatposts.chat_room_id
 INNER JOIN users
 ON chatposts.owner_user_id = users.user_id
 WHERE chatposts.is_deleted = 0 
 AND   users.is_deleted = 0
-GROUP BY chat_name;
+GROUP BY chat_name
+ORDER BY '投稿数';
