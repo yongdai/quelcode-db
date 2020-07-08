68e06b97d88d25232921d95ef9e97810daa07c15
YONGDAI HAN <YONG@YONGDAIs-Macbook-Pro.local>
2020-07-08 14:15:54 +0900

    FROMの前で改行。理論値を一致に変更

--- db/db-challenge3/db-008/db-008.sql
+++ db/db-challenge3/db-008/db-008.sql
@@ -1,8 +1,9 @@
-SELECT nickname, chat_name, DATE_FORMAT(joined_at, '%Y/%m/%d') AS joined_at FROM chatmembers 
+SELECT nickname, chat_name, DATE_FORMAT(joined_at, '%Y/%m/%d') AS joined_at 
+FROM chatmembers 
 INNER JOIN users
 ON chatmembers.member_user_id = users.user_id 
 INNER JOIN chatrooms
 ON chatrooms.chat_room_id = chatmembers.chat_room_id 
-WHERE users.is_deleted != 1 
-AND chatrooms.is_deleted != 1
+WHERE users.is_deleted = 0 
+AND chatrooms.is_deleted = 0
 ORDER BY joined_at ASC;
