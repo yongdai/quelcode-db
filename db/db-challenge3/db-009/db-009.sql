90e8b002ff0502a24b87098c374427fb123ae741
YONGDAI HAN <YONG@YONGDAIs-Macbook-Pro.local>
2020-07-08 16:14:06 +0900

    ORDER BYに降順を追加

--- db/db-challenge3/db-009/db-009.sql
+++ db/db-challenge3/db-009/db-009.sql
@@ -1,9 +1,9 @@
-SELECT COUNT(chat_post_id) AS '投稿数', chat_name FROM chatrooms
-INNER JOIN chatposts
+SELECT COUNT(chat_post_id) AS `投稿数`, chat_name FROM chatrooms
+LEFT JOIN chatposts
 ON chatrooms.chat_room_id = chatposts.chat_room_id
-INNER JOIN users
+LEFT JOIN users
 ON chatposts.owner_user_id = users.user_id
 WHERE chatposts.is_deleted = 0 
 OR   users.is_deleted = 0
 GROUP BY chat_name
-ORDER BY '投稿数';
+ORDER BY `投稿数` DESC;
