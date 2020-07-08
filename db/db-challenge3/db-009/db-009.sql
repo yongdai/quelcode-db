dc4bfb0bc89f26f33d10ad8335ac5c6bbdf9135f
YONGDAI HAN <YONG@YONGDAIs-Macbook-Pro.local>
2020-07-08 15:17:07 +0900

    WHERE句の条件式をANDからORに変更

--- db/db-challenge3/db-009/db-009.sql
+++ db/db-challenge3/db-009/db-009.sql
@@ -4,6 +4,6 @@ ON chatrooms.chat_room_id = chatposts.chat_room_id
 INNER JOIN users
 ON chatposts.owner_user_id = users.user_id
 WHERE chatposts.is_deleted = 0 
-AND   users.is_deleted = 0
+OR   users.is_deleted = 0
 GROUP BY chat_name
 ORDER BY '投稿数';
