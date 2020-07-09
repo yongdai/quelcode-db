eac1cfefa727d76d8f959c968687135c005530a7
YONGDAI HAN <YONG@YONGDAIs-Macbook-Pro.local>
2020-07-09 11:48:59 +0900

    SQLの文末に;追加

--- db/db-challenge3/db-011/db-011.sql
+++ db/db-challenge3/db-011/db-011.sql
@@ -3,6 +3,6 @@ LOCK TABLES tasks WRITE;
 UPDATE tasks
 SET is_completed = 1, updated_at = NOW(), update_user_id = 1
 WHERE created_at BETWEEN '2020/04/26 09:00:00' AND '2020/04/26 11:30:00'
-AND tasks.is_deleted = 0
+AND tasks.is_deleted = 0;
 UNLOCK TABLES;
 COMMIT;
