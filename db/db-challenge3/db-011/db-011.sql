527d6c1834dba4522d1d525b75bb61ed4837cd08
YONGDAI HAN <YONG@YONGDAIs-Macbook-Pro.local>
2020-07-09 11:42:53 +0900

    更新時に更新ユーザーを変更すうるように修正。検索条件から更新者がadminであることを削除

--- db/db-challenge3/db-011/db-011.sql
+++ db/db-challenge3/db-011/db-011.sql
@@ -1,9 +1,8 @@
 BEGIN;
 LOCK TABLES tasks WRITE;
 UPDATE tasks
-SET is_completed = 1, updated_at = NOW()
+SET is_completed = 1, updated_at = NOW(), update_user_id = 1
 WHERE created_at BETWEEN '2020/04/26 09:00:00' AND '2020/04/26 11:30:00'
-AND tasks.is_deleted != 1
-AND update_user_id = 1;
+AND tasks.is_deleted = 0
 UNLOCK TABLES;
 COMMIT;
